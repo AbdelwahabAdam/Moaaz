d2b9a65a769aeec36cf54c80a465b6f2b1df86cf
AbdelwahabAdam <63171603+AbdelwahabAdam@users.noreply.github.com>
2022-07-08 13:06:01 +0200

    Add files via upload
    after checking all code is now compiled

---
+++ Moaaz/Task1/task1.py/task1.py.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+
+int In1=13;
+int Out1=12;
+int In2=11;
+int Out2=10;
+int ServoPin=9;
+Servo myservo;  
+
+void setup() {
+  pinMode(In1, INPUT);
+  pinMode(In2, INPUT);
+  pinMode(Out1, OUTPUT);
+  pinMode(Out2, OUTPUT);
+  myservo.attach(ServoPin);
+
+
+}
+
+void loop() {
+  if (In1 == "HIGH") {
+    digitalWrite(Out1,HIGH);
+    //myservo.write(pos); 
+    }
+
+   if (In2 == "HIGH") {
+    digitalWrite(Out2,HIGH);
+    //myservo.write(pos); 
+    }
+}
--- Moaaz/Task4/Task4/Task4.ino
+++ Moaaz/Task4/Task4/Task4.ino
@@ -6,6 +6,10 @@ int Digit_1 = 0;
 int Digit_2 = 0;
 int Digit_3 = 0;
 int Digit_4 = 0;
+
+int out = 0 ;
+
+
 LiquidCrystal lcd( 8,  9,  4,  5,  6,  7);
 
 void setup() {
@@ -27,19 +31,20 @@ void loop() {
  lcd.setCursor(10,1);
  
  if (x < 60) {
-    Digit_1 + = 1;
+    Digit_1 += 1;
    lcd.print (String(Digit_1) +String(Digit_2)+String(Digit_3)+String(Digit_4) );
  }
  else if (x < 200) {
-      Digit_2 + = 1;
+      Digit_2 += 1;
    lcd.print (String(Digit_1) +String(Digit_2)+String(Digit_3)+String(Digit_4) );
  }
  else if (x < 400){
-      Digit_3 + = 1;
+      Digit_3 += 1;
    lcd.print (String(Digit_1) +String(Digit_2)+String(Digit_3)+String(Digit_4) );
  }
  else if (x < 600){
-      Digit_4 + = 1;
+  
+      Digit_4 += 1;
    lcd.print (String(Digit_1) +String(Digit_2)+String(Digit_3)+String(Digit_4) );
  }
  else if (x < 800){
